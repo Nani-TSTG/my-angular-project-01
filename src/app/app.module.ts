d4ffd8877c46a2bc3377e0740d4bf28c70169632
Nani-TSTG <sasiwimon131967@gmail.com>
2024-08-30 11:10:15 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,25 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
-import { Observable } from 'rxjs';
-
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'my-angular-project-1';
-  responseData$: Observable<any> | undefined;
-  loadData: boolean = false;
-
+  title = 'my_angular_project_1';
+  responseData: any;
+  loadData : Boolean = false;
   constructor(private http: HttpClient){}
 
-  getData(){
-    this.responseData$ = this.http.get('http://jsonplaceholder.typicode.com/users');
-    this.loadData = true;
+    getData(){
+      this.http.get('https://jsonplaceholder.typicode.com/users').subscribe
+      ((data)=>{
+        this.responseData = data;
+        console.log(data);
+        this.loadData = true;
+      })
+    }
   }
-}
